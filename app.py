63a4b4fad3c5769f034f37747638d479f5194206
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-03-09 18:42:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from sklearn import metrics
 from flask import Flask, request, render_template
 import pickle
 
-app = Flask("__name__")
+app = Flask(__name__,template_folder='template')
 
 
 
@@ -39,17 +39,23 @@ def predict():
     
     
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
+    new_df = pd.DataFrame(data, columns = ['mean', 'max', 'kurtosis', 'variance', 'onenorm'])
     
     single = model.predict(new_df)
     probablity = model.predict_proba(new_df)[:,1]
     
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
+    if single==0:
+        o1 = "The pump is in health condition"
         o2 = "Confidence: {}".format(probablity*100)
+    elif single==1:
+        o1 = "The pump is in severe condition"
+        o2 = "Confidence: {}".format(probablity*100)  
+    elif single==2:
+        o1 = "The pump is in mild condition"
+        o2 = "Confidence: {}".format(probablity*100) 
     else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
+        o1 = "The pump is in mild condition"
+        o2 = "Confidence: {}".format(probablity*100) 
         
     return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
     
